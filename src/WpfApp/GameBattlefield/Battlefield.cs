bb6e2a7089664644b4714738e8d747f22ab90f8c
Romez <Romez1990@gmail.com>
2021-06-17 15:46:51 +0300

    temp

--- src/WpfApp/GameBattlefield/Battlefield.cs
+++ src/WpfApp/GameBattlefield/Battlefield.cs
@@ -2,53 +2,54 @@
 using System.Collections.Immutable;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows.Data;
 using Core.Field;
 using Core.Geometry;
-using WpfApp.GameBoard;
 using WpfApp.Toolkit;
 
 namespace WpfApp.GameBattlefield {
     public class Battlefield : Bindable {
         public Battlefield(IEnumerable<Ship> ships) {
+            FieldSize = Field.Size.Map(CalculateOffset) + GridThickness;
+
             // var shipsList = ships.ToList();
             // _ships = new(shipsList);
-            ShipRectangles = new(ships.Map(ToShipRectangle));
-            Grid = MakeGrid();
-            FieldSize = Field.Size.Map(CalculateGridOffset) + GridThickness;
+            GraphicObjects = new() {
+                new CollectionContainer { Collection = CreateGridLines() },
+                new CollectionContainer { Collection = _shipRectangles = new(ships.Map(ToShipRectangle)) },
+            };
         }
-
-        // private readonly ObservableCollection<Ship> _ships;
-
-        private ShipRectangle ToShipRectangle(Ship ship) =>
-            new(ship, CalculateGridOffset);
-
-        public Vector FieldSize { get; }
 
         public const int CellSize = 50;
         public const int GridThickness = 1;
 
-        public ObservableCollection<ShipRectangle> ShipRectangles { get; }
+        public Vector FieldSize { get; }
+
+        public CompositeCollection GraphicObjects { get; }
+
+        private ImmutableArray<GridRectangle> CreateGridLines() =>
+            CreateGridLinesByOrientation(Field.Size.X, Orientation.Horizontal)
+                .Concat(CreateGridLinesByOrientation(Field.Size.Y, Orientation.Vertical))
+                .ToImmutableArray();
+
+        private IEnumerable<GridRectangle> CreateGridLinesByOrientation(int count, Orientation orientation) =>
+            Enumerable.Range(0, count + 1)
+                .Map(CalculateOffset)
+                .Map(offset => new GridRectangle(orientation, offset, CalculateOffset(count), GridThickness));
+
+        // private readonly ObservableCollection<Ship> _ships;
+
+        private readonly ObservableCollection<ShipRectangle> _shipRectangles;
+
+        private ShipRectangle ToShipRectangle(Ship ship) =>
+            new(ship, CalculateOffset);
 
         public void AddShip(Ship ship) {
             // _ships.Add(ship);
-            ShipRectangles.Add(ToShipRectangle(ship));
+            _shipRectangles.Add(ToShipRectangle(ship));
         }
 
-        public ImmutableArray<GridRectangle> Grid { get; }
-
-        private ImmutableArray<GridRectangle> MakeGrid() =>
-            CreateGridLines(Field.Size.X, Orientation.Horizontal)
-                .Concat(CreateGridLines(Field.Size.Y, Orientation.Vertical))
-                .ToImmutableArray();
-
-        private IEnumerable<GridRectangle> CreateGridLines(int count, Orientation orientation) =>
-            Enumerable.Range(0, count + 1)
-                .Map(CalculateGridOffset)
-                .Map(offset => new GridRectangle(orientation, offset, CalculateGridOffset(count), GridThickness));
-
-
-
-        private int CalculateGridOffset(int i) =>
+        private int CalculateOffset(int i) =>
             i * (CellSize + GridThickness);
     }
 }
